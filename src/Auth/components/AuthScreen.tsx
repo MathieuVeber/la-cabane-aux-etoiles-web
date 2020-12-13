4f5c7941777d421271d100f086f94c1cd8a7efe6
Thomas Falcone <32480223+ThomasF34@users.noreply.github.com>
2020-12-14 00:49:08 +0100

    Add Login component

--- src/Auth/components/AuthScreen.tsx
+++ src/Auth/components/AuthScreen.tsx
@@ -1,27 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { login } from "../actions";
 import { authSelector } from "../reducer";
 import { teapot } from "../../api/auth";
+import ReactBootstrap from "react-bootstrap";
+import Form from "react-bootstrap/esm/Form";
+import Button from "react-bootstrap/esm/Button";
 
 function AuthScreen() {
   const { loading, error, success } = useSelector(authSelector);
   const dispatch = useDispatch();
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    dispatch(login({ email, password }));
+  };
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
   return (
     <>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="emailForm">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="john.wick@mail.com"
+            onChange={handleEmailChange}
+            value={email}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="passwordForm">
+          <Form.Label>Mot de Passe</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="********"
+            onChange={handlePasswordChange}
+            value={password}
+          />
+        </Form.Group>
+        <Button variant="outline-info" type="submit" disabled={loading}>
+          Se connecter
+        </Button>
+      </Form>
+
       <div>loading: {`${loading}`}</div>
       <div>error: {`${error}`}</div>
       <div>success: {`${success}`}</div>
-
-      <button
-        onClick={() =>
-          dispatch(login({ email: "vincent", password: "vincent" }))
-        }
-      >
-        Login as Vincent
-      </button>
-      <button onClick={() => teapot()}>Are you a teapot ?</button>
     </>
   );
 }
